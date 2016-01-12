0e549a60ca1b54a1f147cc3816fbfe97e4042ee4
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2016-01-12 22:11:32 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String args[]) {
+        int x;
+        x = 4 + 6;
+        // --------------------------------------------------
+        String s = "test string";
+        // --------------------------------------------------
+        String y1 = "   123   ";
+        int y2 = Integer.parseInt(y1.trim());
+        // --------------------------------------------------
+        char c = 'X'; // '
+        String converted = "" + c; // converted to String
+        // --------------------------------------------------
         System.out.println("Hello World");
+        System.out.println("x=" + x);
+        System.out.println("y2=" + y2);
+        System.out.println("Charcode of 50 is " + (char) 50);
     }
 }
